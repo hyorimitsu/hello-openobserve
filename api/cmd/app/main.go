3aea47bf17fa32b85b6e97b04e50e4a9fcddb148
Hiroki Yorimitsu <52403055+hyorimitsu@users.noreply.github.com>
2023-07-28 23:54:47 +0900

    update main.go (use otel helper)

--- api/cmd/app/main.go
+++ api/cmd/app/main.go
@@ -1,20 +1,65 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"log"
 	"net/http"
-	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho"
+	"go.opentelemetry.io/otel"
+
+	"github.com/hyorimitsu/hello-openobserve/api/config"
+	"github.com/hyorimitsu/hello-openobserve/api/pkg/tel"
 )
 
+var tracer = otel.Tracer("github.com/hyorimitsu/hello-openobserve/api")
+
 func main() {
-	baseUrl := os.Getenv("BASE_URL")
-	port := os.Getenv("PORT")
+	option := tel.OTelHttpConfig{
+		ExporterConfig: tel.OTelHttpExporterConfig{
+			Host:          config.OTelExporterHost,
+			Port:          config.OTelExporterPort,
+			UrlPath:       config.OTelExporterUrlPath,
+			Authorization: config.OTelExporterAuthorization,
+			IsEnabledSSL:  config.Env != "local",
+		},
+		AttributesConfig: tel.OTelHttpAttributesConfig{
+			Name:        config.Name,
+			Version:     config.Version,
+			Environment: config.Env,
+		},
+	}
 
-	http.HandleFunc(baseUrl+"/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Println("[Info] Called API")
-		fmt.Fprintf(writer, "Hello, world!")
-	})
+	tracerProvider, err := tel.InitOTelHttpTracer(option)
+	if err != nil {
+		fmt.Println("[Error] Unable to initialize OTel HTTP exporter")
+		return
+	}
+	defer func() {
+		if err := tracerProvider.Shutdown(context.Background()); err != nil {
+			fmt.Println("[Error] Unable to shutting down tracer provider: ", err)
+		}
+	}()
 
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(otelecho.Middleware(config.Name))
+
+	e.GET(config.BaseUrl+"/hello", hello)
+
+	e.Logger.Fatal(e.Start(":" + config.Port))
+}
+
+func hello(ctx echo.Context) error {
+	c := ctx.Request().Context()
+
+	_, span := tracer.Start(c, "hello")
+	defer span.End()
+
+	fmt.Println("[Info] Called API")
+	return ctx.String(http.StatusOK, "Hello, World!")
 }
